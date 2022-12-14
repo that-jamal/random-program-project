a9a3bff6aa65cf96fd4888458a9af4eedcd580af
that-jamal <varg.jamil@gmail.com>
2022-12-14 15:32:14 +0100

    added full screen

--- main.js
+++ main.js
@@ -122,5 +122,17 @@ function linkback() {
         document.querySelector("ul").appendChild(li);
     }
 }
+
+var elem = document.getElementById("game");
+function openFullscreen() {
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) { /* Safari */
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE11 */
+        elem.msRequestFullscreen();
+    }
+}
+
 linkback()
 srcChanger()
